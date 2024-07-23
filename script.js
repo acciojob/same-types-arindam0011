24c7a93d4bba56c9160910ada41fb9440d3d2363
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-23 06:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
 function isSameType(value1, value2) {
-  
-    let type1;
-    let type2;
-  if(value1.charAt(0) == "'"|| value1.charAt(0) == '"'){
-    type1 = "string";
-  }
-  else{
-    type1 = "number";
-  }
-  if(value2.charAt(0) == "'"|| value2.trim().charAt(0) == '"'){
-    type2 = "string";
-  }
-  else{
-    type2 = "number";
-  }
-
-  if(type1 == type2){
-    return true;
-  }
-  else{
-    return false;
-  }
-
-
-
+	
   if (Number.isNaN(value1) && Number.isNaN(value2)) {
-    return true;
+    return typeof value1 == typeof value2;
 }
 return typeof value1 === typeof value2;
   
